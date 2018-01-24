f9d597747ce019ab9b63ce73564a69b5c0ad5eb3
Laslyle <30777117+Laslyle@users.noreply.github.com>
2018-01-24 15:15:42 +0800

    基本功能完成

--- src/com/brainstrongtech/hrmis/ui/EmpAddUI.java
+++ src/com/brainstrongtech/hrmis/ui/EmpAddUI.java
@@ -12,6 +12,7 @@ import java.util.List;
 import com.brainstrongtech.hrmis.dao.EmployeeDao;
 import com.brainstrongtech.hrmis.dao.EmployeeDaoTxtImpl;
 import com.brainstrongtech.hrmis.domain.Employee;
+import com.brainstrongtech.hrmis.exception.BlankEntryException;
 import com.brainstrongtech.hrmis.utils.SysUtils;
 
 
@@ -40,7 +41,12 @@ public class EmpAddUI implements BaseUI {
          
 		while (true) {
 			System.out.println("Enter employee 3 digit payroll number");
+			try{
 			entry = SysUtils.getEntry("No payroll number entered 每 try again", false);
+			}catch(BlankEntryException e){
+				  SysUtils.pause(e.getMessage());
+				  continue;
+				}
 			int flag = 0;
 			for (Employee s : empList) {
 				if (s.getPayrollNo().equals(entry)) {
@@ -58,8 +64,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 		System.out.println("Enter Phone Number (02-12345678)");
+		try{
 		entry = SysUtils.getEntry("No phone number entered 每 try again", false);
-
+		}catch(BlankEntryException e){
+			  SysUtils.pause(e.getMessage());
+			  continue;
+			}
           if(entry.matches("^0[2-8]{1}-[1-9]{1}[0-9]{7}$")){
 		em.setTelephoneNum(entry);;
 		break;}
@@ -69,8 +79,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 			System.out.println("Enter Last Name");
+			try{
 			entry = SysUtils.getEntry("No last name entered 每 try again", false);
-	
+			}catch(BlankEntryException e){
+				  SysUtils.pause(e.getMessage());
+				  continue;
+				}
 		    if(entry.matches("[a-zA-Z]*\\s*")){
 				em.setLastName(entry);
 			break;}
@@ -80,7 +94,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 			System.out.println("Enter First Name");
+			try{
 			entry = SysUtils.getEntry("No First name entered 每 try again", false);
+			}catch(BlankEntryException e){
+				  SysUtils.pause(e.getMessage());
+				  continue;
+				}
 		 if(entry.matches("[a-zA-Z]*\\s*")){
 				em.setFirstName(entry);
 			break;}
@@ -90,7 +109,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 			System.out.println("Enter Middle Init");
+			try{
 			entry = SysUtils.getEntry("No Middle Init entered 每 try again", false);
+			}catch(BlankEntryException e){
+				  SysUtils.pause(e.getMessage());
+				  continue;
+				}
 			if(entry.matches("[a-zA-Z]*\\s*")){
 				em.setInitial(entry);
 			break;}
@@ -100,7 +124,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 			System.out.println("Enter Dept #");
+			try{
 			entry = SysUtils.getEntry("No Dept # entered 每 try again", false);
+		}catch(BlankEntryException e){
+			  SysUtils.pause(e.getMessage());
+			  continue;
+			}
 			 if(entry.matches("^[\\d]{1,}$")){
 				em.setDeptNo(Integer.parseInt(entry));
 			break;}
@@ -110,8 +139,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 			System.out.println("Enter Job Title");
+			try{
 			entry = SysUtils.getEntry("No Job title entered 每 try again", false);
-
+			}catch(BlankEntryException e){
+				  SysUtils.pause(e.getMessage());
+				  continue;
+				}
 		   if(entry.matches("[a-zA-Z]*\\s*")){
 				em.setJobTitle(entry);
 			break;}
@@ -121,8 +154,12 @@ public class EmpAddUI implements BaseUI {
 		
 		while(true){
 			System.out.println("Enter Date Hired (dd-mm-yyyy)");
+			try{
 			entry = SysUtils.getEntry("No date hired entered 每 try again", false);
-			
+			}catch(BlankEntryException e){
+				  SysUtils.pause(e.getMessage());
+				  continue;
+				}
 				System.out.println("No date hired entered 每 try again");
 			if(entry.matches("(((0[1-9]|[12][0-9]|3[01])-((0[13578]|1[02]))|((0[1-9]|[12][0-9]|30)-(0[469]|11))|(0[1-9]|[1][0-9]|2[0-8])-(02))-([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3}))|(29/02/(([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00)))")){
 				try {
@@ -143,7 +180,8 @@ public class EmpAddUI implements BaseUI {
 		
 		for(Employee s:empList){
 				try {
-					fw.write(s.getPayrollNo()+":"+s.getTelephoneNum()+":"+s.getLastName()+":"+s.getFirstName()+":"+s.getInitial()+":"+s.getDeptNo()+":"+s.getJobTitle()+":"+s.getHiringDate()+"\r\n");
+					//fw.write(s.getPayrollNo()+":"+s.getTelephoneNum()+":"+s.getLastName()+":"+s.getFirstName()+":"+s.getInitial()+":"+s.getDeptNo()+":"+s.getJobTitle()+":"+s.getHiringDate()+"\r\n");
+				fw.write(s.toString()+"\r\n");
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
--- src/com/brainstrongtech/hrmis/ui/EmpDelete.java
+++ src/com/brainstrongtech/hrmis/ui/EmpDelete.java
@@ -3,6 +3,16 @@
  */
 package com.brainstrongtech.hrmis.ui;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import com.brainstrongtech.hrmis.dao.EmployeeDao;
+import com.brainstrongtech.hrmis.dao.EmployeeDaoTxtImpl;
+import com.brainstrongtech.hrmis.domain.Employee;
+import com.brainstrongtech.hrmis.utils.SysUtils;
+
+
 /**
  * @author Hong
  *
@@ -13,9 +23,60 @@ public class EmpDelete implements BaseUI {
 	 * @see com.brainstrongtech.hrmis.ui.BaseUI#render()
 	 */
 	@Override
-	public void render() {
+	public void render() throws IOException {
 		// TODO Auto-generated method stub
-
+		final String DATA_FILE = "d:\\records.txt";
+		FileWriter fw;
+			
+		EmployeeDao empDao = new EmployeeDaoTxtImpl();
+		List<Employee> empList = empDao.loadEmps();
+		String entry = null;
+		int flag=0;
+		System.out.println("ÐË°î×ÊÑ¶ ¨C Employee Records:\r\n====================================");
+	    while(true){
+		System.out.println("Enter employee¡¯s 3 digit payroll number to enable record deletion");
+		entry = SysUtils.getEntry("No payroll number entered ¨C try again", false);
+		 if(entry.matches("^[\\d]{3}$")){
+			 break;}
+		 else
+			 System.out.println("Payroll number can contain only numerical characte");
+	    }
+		
+		 
+			 for (int i = 0; i < empList.size(); i++) {
+		            if (empList.get(i).getPayrollNo().equals(entry)) {
+		            	System.out.println("Confirm record deletion, (y)es or (n)o, ");
+		            	entry = SysUtils.getEntry("No selection", false);
+		            	if(entry.equalsIgnoreCase("y")){
+		            	   empList.remove(i);
+		            	 flag=1;  
+		            	}
+		            	else 
+		            	{
+		            		flag=2;
+		            		break;
+		            	}
+		            }
+		        }
+			 
+			 if(flag==0)
+         		{
+				 System.out.println("Employee record for "+entry+" not found");
+	            	SysUtils.pause("Press Enter to continue");
+         		}
+			 else if(flag==1){
+				 fw = new FileWriter(DATA_FILE, false);
+				for(Employee s:empList){
+					//fw.write(s.getEmpPayNumber()+":"+s.getTelNumber()+":"+s.getLastName()+":"+s.getFirstName()+":"+s.getInitName()+":"+s.getDepNumber()+":"+s.getJobTitle()+":"+s.getDate()+"\r\n");
+					fw.write(s.toString()+"\r\n");
+			}
+				
+			fw.flush();
+	        fw.close();
+	        System.out.println("Record deleted");}
+			 else
+				 SysUtils.pause("Press Enter to continue");
+				 
 	}
 
 }
