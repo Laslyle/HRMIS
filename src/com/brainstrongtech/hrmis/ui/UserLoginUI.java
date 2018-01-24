c332d43a36c818ad1689b167dd4bc4d353386439
Laslyle <30777117+Laslyle@users.noreply.github.com>
2018-01-24 19:57:25 +0800

    部分登录代码

---
+++ src/com/brainstrongtech/hrmis/ui/UserLoginUI.java
@@ -0,0 +1,21 @@
+package com.brainstrongtech.hrmis.ui;
+
+import java.io.IOException;
+
+import com.brainstrongtech.hrmis.domain.Employee;
+
+public class UserLoginUI implements BaseUI {
+
+	@Override
+	public void render() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(Employee e) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
